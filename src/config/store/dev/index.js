8d1ccbffbafcae2ec1484fb8f332d27d06e5f659
zhoukaiyue <1301524439@qq.com>
2022-08-12 11:10:21 +0800

    fix：更新代码注释

--- src/config/store/dev/index.js
+++ src/config/store/dev/index.js
@@ -4,15 +4,18 @@
  * @Author: zhoukai
  * @Date: 2022-08-12 09:51:01
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-08-12 10:43:18
+ * @LastEditTime: 2022-08-12 11:08:57
  */
 
+/**
+ * dev 就是你的 模块名
+ */
 export const dev = {
     /**
-     * 声明分模块的store时加上namespaced:true,使用模块中的mutations、getters、actions时候，要加上模块名
-     * 提交mutations：格式：store.commit(模块名/模块中的mutations)
-     * 提交dispatch：格式：store.dispatch(模块名/模块中的dispatch)
-     * 获取状态属性：格式：store.state.模块名.模块属性
+     * 声明分模块的store时加上namespaced:true,使用模块中的mutations、getters、actions时候，要加上模块名(这里的模块名就是 dev)
+     * 提交mutations：格式：store.commit(dev/模块中的mutations)
+     * 提交dispatch：格式：store.dispatch(dev/模块中的dispatch)
+     * 获取状态属性：格式：store.state.dev.模块属性
      */
     namespaced: true,
     /**
