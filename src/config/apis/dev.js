7b4bd7a6ddd97ada8f97a9f9edc216c851991944
zhoukaiyue <1301524439@qq.com>
2022-11-22 10:27:34 +0800

    style: 代码格式化

--- src/config/apis/dev.js
+++ src/config/apis/dev.js
@@ -4,10 +4,13 @@
  * @Author: zhoukai
  * @Date: 2022-08-10 17:02:54
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-08-10 17:03:21
+ * @LastEditTime: 2022-11-22 10:17:10
  */
 import { $post } from '@/packages/request';
 
 export const getList = (params) => {
-    return $post('/mock/14/demo/getList', params);
+    return $post('/mock/14/demo/getList', params, {
+        retryDelay: 4000, //当前请求重试间隔设置为4秒
+        retryFrequency: 2 //当前请求重试次数（重试频率）2次
+    });
 };
