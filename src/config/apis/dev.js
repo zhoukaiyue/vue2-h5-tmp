1188fcee94da4bcd1157533fd0f5a9c8a3460536
zhoukaiyue <1301524439@qq.com>
2022-12-09 15:33:24 +0800

    feat: axios请求库判断不同HTTP状态码

--- src/config/apis/dev.js
+++ src/config/apis/dev.js
@@ -4,12 +4,15 @@
  * @Author: zhoukai
  * @Date: 2022-08-10 17:02:54
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-11-22 10:17:10
+ * @LastEditTime: 2022-12-09 15:32:00
  */
 import { $post } from '@/packages/request';
 
 export const getList = (params) => {
     return $post('/mock/14/demo/getList', params, {
+        enableCancelModel: true,
+        enableRetryModel: true,
+        enableErrorMessage: true,
         retryDelay: 4000, //当前请求重试间隔设置为4秒
         retryFrequency: 2 //当前请求重试次数（重试频率）2次
     });
