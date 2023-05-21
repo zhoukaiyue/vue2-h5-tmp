c3e3bd967c94b1fd0869b45c478f98a18ad68da7
zhoukaiyue <1301524439@qq.com>
2023-05-21 16:32:53 +0800

    feat:add BaseQrcode  基础组件
    一个用于生成和显示 QR 码的基础组件

---
+++ src/resources/components/base-qrcode/README.md
@@ -0,0 +1,29 @@
+# 一个用于生成和显示 QR 码的基础组件
+
+## props
+
+|  属性  |        说明        |  类型  | 默认值 |
+| :----: | :----------------: | :----: | :----: |
+| value  | 生成的 QR 码的内容 | String |   -    |
+|  size  |        尺寸        | Number |  128   |
+| margin |      空白边界      | Number |   2    |
+
+## 使用方法
+
+```vue
+<template>
+    <div>
+        <base-qrcode value="https://example.com" size="200"></base-qrcode>
+    </div>
+</template>
+
+<script>
+import BaseQrcode from '@/resources/components/base-qrcode';
+
+export default {
+    components: {
+        BaseQrcode
+    }
+};
+</script>
+```
---
+++ src/resources/components/base-qrcode/index.vue
@@ -0,0 +1,61 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: zhoukai
+ * @Date: 2023-05-21 16:14:53
+ * @LastEditors: zhoukai
+ * @LastEditTime: 2023-05-21 16:25:42
+-->
+<template>
+    <div class="base-qrcode">
+        <canvas ref="qrcodeCanvas"></canvas>
+    </div>
+</template>
+
+<script>
+import QRCode from 'qrcode';
+
+export default {
+    name: 'BaseQrcode',
+    props: {
+        value: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: Number,
+            default: 128
+        },
+        margin: {
+            type: Number,
+            default: 2
+        }
+    },
+    mounted() {
+        this.generateQRCode();
+    },
+    watch: {
+        value() {
+            this.generateQRCode();
+        }
+    },
+    methods: {
+        generateQRCode() {
+            const canvas = this.$refs.qrcodeCanvas;
+            canvas.width = this.size;
+            canvas.height = this.size;
+            QRCode.toCanvas(canvas, this.value, { width: this.size, margin: this.margin }, (error) => {
+                if (error) {
+                    console.error('Failed to generate QR code', error);
+                }
+            });
+        }
+    }
+};
+</script>
+
+<style scoped>
+.base-qrcode {
+    display: inline-block;
+}
+</style>
